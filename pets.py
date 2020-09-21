b197f31711be1d0bcd06a6ab466715b50f4670a4
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2020-09-21 13:39:22 -0300

    pagina 177 - valores de retorno

--- pets.py
+++ pets.py
@@ -6,3 +6,12 @@ while 'cat' in pets:
 	pets.remove('cat')
 	
 print(pets)
+
+def describe_pet(animal_type, pet_name):
+	"""Exibe informações sobre um animal de estimação"""
+	print("\nI have a "  + animal_type + "'s name is "+
+		pet_name.title() + ".")
+
+describe_pet('hamister', 'harry')
+describe_pet('dog', 'willie')
+
