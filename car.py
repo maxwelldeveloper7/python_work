504f709509f86406149c35fbc25ea1ad4a76eba0
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2020-09-27 15:02:08 -0300

    pagina 210 - herança

---
+++ car.py
@@ -0,0 +1,45 @@
+class Car():
+	"""Uma tentativa Simples de representar um carro."""
+
+	def __init__(self, make, model, year):
+		"""Inicializa os atributos que descrevem um carro."""
+		self.make = make
+		self.model = model
+		self.year = year
+		self.odometer_reading = 0
+
+	def get_descriptive_name(self):
+		"""Devolve um nome descrito, formatado de modo elegante."""
+		long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+		return long_name.title()
+
+	def read_odometer(self):
+		"""Exibe uma frase que mostra a milhagem do carro."""
+		print("This car has " + 
+			str(self.odometer_reading) + " miles on it.")
+
+	def update_odometer(self, mileage):
+		"""Define o valor de leitura do hodômetro com o valor 
+		especificado"""
+		if mileage >= self.odometer_reading:
+			self.odometer_reading = mileage
+		else:
+			print("You can't roll back an odometer!")
+
+	def increment_odometer(self, miles):
+		"""Soma a quantidade especificada ao valor de leitura do 
+		hodômetro."""
+		self.odometer_reading += miles
+
+
+my_new_car = Car('audi', 'a4', 2016)
+print(my_new_car.get_descriptive_name())
+my_new_car.update_odometer(23)
+my_new_car.read_odometer()
+
+my_used_car = Car('subaru', 'outoback', 2013)
+print(my_used_car.get_descriptive_name())
+my_used_car.update_odometer(23500)
+my_used_car.read_odometer()
+my_used_car.increment_odometer(100)
+my_used_car.read_odometer()
--- dog.py
+++ dog.py
@@ -15,7 +15,14 @@ class Dog():
 
 
 my_dog = Dog('willie', 6)
+your_dog = Dog('lucy', 3)
+
 print("My dog's name is " + my_dog.name.title() + ".")
 print("My dog's " + str(my_dog.age) + " years old.")
 my_dog.sit()
 my_dog.roll_over()
+
+print("My dog's name is " + your_dog.name.title() + ".")
+print("My dog's " + str(your_dog.age) + " years old.")
+your_dog.sit()
+your_dog.roll_over()
