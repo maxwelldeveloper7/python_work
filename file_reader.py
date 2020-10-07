e43d94d27860ce5c3f39cc0f293cb8f373d3c9e1
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2020-10-07 06:21:01 -0300

    Página 237 - Escrevendo dados em um arquivo vazio

--- file_reader.py
+++ file_reader.py
@@ -1,8 +1,15 @@
-filename = 'pi_digits.txt'
+filename = 'pi_million_digits.txt'
 with open(filename) as file_object:
 	lines = file_object.readlines()
+	pi_string = ''
 	
 for line in lines:
-	print(line.rstrip())
-	
-print(lines)
+	pi_string += line.rstrip()
+
+birthday = input("Enter your birthday, in the form mmddyy: ")
+
+if birthday in pi_string:
+	print("You birthday appears in the million digits of pi!")
+else:
+	print("You birthday does not appear in the first million digits of"+
+	" pi.")
