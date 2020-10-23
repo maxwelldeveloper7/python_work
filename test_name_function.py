cce6d735722548c6734fe3d00e15796924b329a0
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2020-10-22 21:19:18 -0300

    página 264 - Testando uma classe

---
+++ test_name_function.py
@@ -0,0 +1,19 @@
+import unittest
+from name_function import get_formatted_name
+
+class NamesTesCase(unittest.TestCase):
+	"""Testes para 'name_function.py'."""
+	
+	def test_first_last_name(self):
+		"""Nomes como 'Janis Joplin' funcionam?"""
+		formatted_name = get_formatted_name('janis', 'joplin')
+	
+		self.assertEqual(formatted_name, 'Janis Joplin')
+		
+	def test_first_middle_last_name(self):
+		"""Nomes como 'Wolfgang Amadeus Mozart' funcionam?"""
+		formatted_name = get_formatted_name('wolfgang', 'mozart', 
+											'amadeus')
+		self.assertEqual(formatted_name, 'Wolfgang Amadeus Mozart')
+	
+unittest.main()
